ccb7bca20e9087d550ee10fc4482e5bf8a9b2b0c
Bruno Debenay <bruno.debenay@groupama.com>
2024-07-26 16:52:55 +0200

    Tests connexion Client

--- MongoDB/MongoDB1.py
+++ MongoDB/MongoDB1.py
@@ -6,7 +6,9 @@ ENDPOINT = "34.244.43.135:27017"
 USERNAME = "admin"
 PASSWORD = "34.244.43.135:27017"
 
-client = MongoClient(f'mongodb://{USERNAME}:{PASSWORD}@{ENDPOINT}/?directConnection=true')
+#client = MongoClient(f'mongodb://{USERNAME}:{PASSWORD}@{ENDPOINT}/?directConnection=true')
+#client = MongoClient(f'mongodb://localhost:27017')
+#client = MongoClient(host="localhost", port=27017)
 
-client.list_database_names ()
+print(client.list_database_names ())
 
